395155c369beaee3bdb18e5204daf90232f6d688
Yoan Ferrufino <yoan@scz-reserva.com>
2026-06-28 01:23:27 +0000

    chore: cleanup temp files

--- migrations/005_payment_qr.sql
+++
@@ -1,3 +0,0 @@
--- Agregar columna para QR de pago por organización
-ALTER TABLE organizations 
-  ADD COLUMN IF NOT EXISTS payment_qr_url TEXT;
--- migrations/006_org_rls_policy.sql
+++
@@ -1,7 +0,0 @@
--- Permitir a cualquiera ver datos básicos de organizaciones (nombre, QR)
--- Esto es necesario para que el QR de pago se muestre en el booking
-CREATE POLICY "Anyone can read org basic info"
-ON public.organizations
-FOR SELECT
-TO anon
-USING (true);
